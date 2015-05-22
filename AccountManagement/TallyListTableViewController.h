11fe59144cf66adbd7a62edea4c3454250311290
lm2343635 <lm2343635@126.com>
2015-05-22 12:17:21 +0800

    none

---
+++ AccountManagement/TallyListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TallyListTableViewController.h
+//  AccountManagement
+//
+//  Created by 李大爷 on 15/5/22.
+//  Copyright (c) 2015年 李大爷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TallyListTableViewController : UITableViewController
+
+@end
